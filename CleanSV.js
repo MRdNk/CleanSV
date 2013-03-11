b6e6ff4ec20d4951671c167da29629179d22b262
Duncan Angus Wilkie (mrdnk) <duncanwilkie@hotmail.com>
2013-03-11 22:57:26 +0000

    Added noTrim options

--- CleanSV.js
+++ CleanSV.js
@@ -9,12 +9,25 @@ function filename (val) {
 	return val
 }
 
+function list (val) {
+	return val.split(',')
+}
+
+var opts = {
+		delim: ','
+	,	headers: true
+}
+
 program
 	.version('0.0.1')
 	.option('-f, --filename [type]', 'filename to pipe to', filename)
+	.option('-t, --noTrim <columns>', 'columns to not trim', list)
 	.parse(process.argv)
 
 if (program.filename) {
-	process.stdin.pipe(CleanSV(null)).pipe(fs.createWriteStream(program.filename))
+	if (program.noTrim) {
+		opts.noTrim = program.noTrim
+	}
+	process.stdin.pipe(CleanSV(opts)).pipe(fs.createWriteStream(program.filename))
 }
 
